7a57d09d28490f0b9413f95a3b5115fb96e029dc
kuresti <55415865+kuresti@users.noreply.github.com>
2025-07-10 14:57:29 -0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
+/************************
+ * Imports
+ ************************/
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeopleComponent } from './people/people.component';
+import { ResearchNotesListComponent } from './research-notes/research-notes-list/research-notes-list.component';
+import { DocumentsComponent } from './documents/documents.component';
 
-const routes: Routes = [];
+/***************************
+ * Routes Definition
+ ***************************/
+const routes: Routes = [
+  { path: '', redirectTo: '/people', pathMatch: 'full' },
+  { path: 'people', component: PeopleComponent },
+  { path: 'research-notes', component: ResearchNotesListComponent },
+  { path: 'documents', component: DocumentsComponent }  
+];
 
+/***************************
+ * Module
+ ***************************/
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
