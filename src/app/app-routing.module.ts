395659833061d8bcc5446519aac4cfacd3bb8436
kuresti <55415865+kuresti@users.noreply.github.com>
2025-07-10 15:37:30 -0600

    Defined child routes and new person button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { PeopleComponent } from './people/people.component';
 import { ResearchNotesListComponent } from './research-notes/research-notes-list/research-notes-list.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { PersonEditComponent } from './people/person-edit/person-edit.component';
+import { PersonDetailComponent } from './people/person-detail/person-detail.component';
 
 /***************************
  * Routes Definition
  ***************************/
 const routes: Routes = [
   { path: '', redirectTo: '/people', pathMatch: 'full' },
-  { path: 'people', component: PeopleComponent },
+  { path: 'people', component: PeopleComponent, children: [
+    { path: 'new', component: PersonEditComponent },
+    { path: ':id', component: PersonDetailComponent },
+    { path: ':id/edit', component: PersonEditComponent }
+  ] },
   { path: 'research-notes', component: ResearchNotesListComponent },
   { path: 'documents', component: DocumentsComponent }  
 ];
